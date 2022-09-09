b59167c2b64e225b5753bbb647608033349f6177
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-09-08 18:55:11 -0600

    completed code

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,17 @@
 // Write your code below
+const isPalindrome = (str) => {
+    const strArr = str.split('');
+    for (let i = 0; i < (strArr.length / 2); i++) {
+        if (strArr[i] !== strArr[strArr.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome('a'));
+console.log(isPalindrome('noon'));
+console.log(isPalindrome('hello'));
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('Racecar'));
+console.log(isPalindrome('asdfjdsa'));
